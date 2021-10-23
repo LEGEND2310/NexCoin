f82d0571841f615735470903ffca2377ef1ad784
divyakumari17 <73510013+divyakumari17@users.noreply.github.com>
2021-10-23 14:55:21 +0530

    Added Comments

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ last_block = blockchain.latest_block
 last_proof_no = last_block.proof_no
 proof_no = blockchain.proof_of_work(last_proof_no)
 
+#Instantiating the blockchain
 blockchain.new_data(
     sender="0",  #it implies that this node has created a new block
     recipient="Legend",  #let's send Legend some coins!
